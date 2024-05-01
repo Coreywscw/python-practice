45999bc648dfa711168289f383fd561184c4407a
Coreywscw <159732880+Coreywscw@users.noreply.github.com>
2024-05-02 10:27:33 +1200

    Created initial quiz

---
+++ main.py
@@ -0,0 +1,10 @@
+#Getting persons name
+__name__= input("What's your name? ")
+#Welcoming person to quiz and telling them the topic
+print("Welcome to the quiz",__name__)
+print("The quiz topic is general knowledge")
+#Asking person the question
+input("What is Obama's last name? ")
+#Tell person correct answer
+print("The correct answer is Obama")
+print("Thank you for playing")
