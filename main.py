d08e0baf0fadf5a9e22c34022e9af39705c1abfb
Coreywscw <159732880+Coreywscw@users.noreply.github.com>
2024-05-07 14:05:45 +1200

    Questions and answers

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print("Welcome to the quiz", name)
 print("The quiz topic is general knowledge")
 
 #Asking person the questions and telling them the answers
-answer1 = input("What is Obama's last name? ")
+answer1 = input("What is Obama's last name? (make sure to capitalize your answer)")
 
 if answer1 == "Obama":  
     print("That's correct, Obama is his last name, very good!")
@@ -15,9 +15,9 @@ else:
     print("That's incorrect, your silly")
     print("The correct answer is Obama")
 
-answer2 = input("What costs more, a trip to space or a coffee from ponsonby? ")
+answer2 = input("What costs more, a trip to space or a coffee from ponsonby? (answer space or coffee, make sure to capitalize your answer) ")
 
-if answer2 == "a coffee from ponsonby":
+if answer2 == "Coffee":
     print("That's correct, You obviously live in Auckland! ")
 
 else:
@@ -25,9 +25,9 @@ else:
     print("The correct answer is a coffee from ponsonby, You obviously don't live in Auckland ")
 
 
-answer3 = input("What is bigger the population of China or the population of road cones around Auckland?")
+answer3 = input("What is bigger the human population of China or the population of road cones around Auckland? (Answer humans or Road cones, make sure to capitalize your answer)")
 
-if answer3 == "the population of road cones around auckland":
+if answer3 == "Road cones":
     print("Very good that is absolutely correct!")
 
 else:
